b90695eac9809bfeae105e94f5b6aa52f1b24a55
vodoanminhhieuvn <vodoanminhhieuvn@gmail.com>
2022-05-28 21:17:52 +0700

    [Too lazy to commit]: Update Login, Core

---
+++ lib/widgets/user_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+
+class UserCard extends StatelessWidget {
+  final User user;
+
+  const UserCard({
+    @required this.user,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ProfileAvatar(imageUrl: user.imageUrl),
+          const SizedBox(width: 6.0),
+          Flexible(
+            child: Text(
+              user.name,
+              style: const TextStyle(fontSize: 16.0),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
